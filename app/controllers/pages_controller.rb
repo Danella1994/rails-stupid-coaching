2a65161f105204eebb97d0ba3a3575d776578936
Danella Kababanda <binaganamiella@gmail.com>
2023-05-22 15:34:17 -0400

    Stipid coaching

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,16 @@
 class PagesController < ApplicationController
-  def ask; end
+  def ask
+  def answer
+    @question = params['question']
+    @answer = ''
+
+  if@question == 'I am going to work'
+    @answer = 'Great'
+  elsif @question.chars.last == '?'
+    @answer = 'silly question get dressed and go to work'
+  else
+    @answer = 'I do not care get dressed and go to work!'
+  end
+  end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get '/ask', to: 'pages#ask'
+  get '/answer', to: 'pages#answer'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "articsles#index"
 end
