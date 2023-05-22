592bc42b2b170569d68f68d7ef76468dbe122bb0
Danella Kababanda <binaganamiella@gmail.com>
2023-05-22 13:45:44 -0400

    stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/ask', to: 'pages#ask'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
